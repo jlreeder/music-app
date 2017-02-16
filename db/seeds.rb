10cd9af52482915e3fcec90515237fc403cbbdcd
Jason Reeder <info@jasonreeder.com>
2017-02-16 15:04:33 -0800

    Correct capitalization

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,5 @@ beatles = Band.create!(name: "The Beatles")
 abbey = Album.create!(name: "Abbey Road",
                       band_id: beatles.id,
                       recording_type: "studio")
-come_together = Track.create!(name: "Come together", album_id: abbey.id, track_type: "regular", lyrics: "Here come old flattop")
+come_together = Track.create!(name: "Come Together", album_id: abbey.id, track_type: "regular", lyrics: "Here come old flattop")
 octopus = Track.create!(name: "Octopus's Garden", album_id: abbey.id, track_type: "regular", lyrics: "I'd like to be, under the sea")
