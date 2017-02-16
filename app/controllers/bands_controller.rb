cb96885dd5e54a459400182b286d7c5ac509e3d7
Jason Reeder <info@jasonreeder.com>
2017-02-16 15:00:06 -0800

    Add basic band routes

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,13 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
+  def new; end
 end
